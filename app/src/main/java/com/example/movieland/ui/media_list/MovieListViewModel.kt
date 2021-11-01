dcaf13e5ffff919cf6953f9e30a92779dbdda9e0
Faizan Haider <haiderfaizan732@gmail.com>
2021-11-01 23:59:08 +0530

    chore: Integrates Kotlin lint check

--- app/src/main/java/com/example/movieland/ui/media_list/MovieListViewModel.kt
+++ app/src/main/java/com/example/movieland/ui/media_list/MovieListViewModel.kt
@@ -90,5 +90,4 @@ class TrendingViewModel @AssistedInject constructor(
     private fun getBollywoodMovies() {
         categoryMedia = movieRepo.fetchBollywoodMoviesPaging().cachedIn(viewModelScope)
     }
-
 }
