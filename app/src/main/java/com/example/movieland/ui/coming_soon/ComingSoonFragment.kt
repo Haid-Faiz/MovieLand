1d8217be7acf5669b8787e9470badf0fd5781248
Haid-Faiz <haiderfaizan732@gmail.com>
2022-06-25 14:52:58 +0530

    chore: Bumps up version to v1.1.0

--- app/src/main/java/com/example/movieland/ui/coming_soon/ComingSoonFragment.kt
+++ app/src/main/java/com/example/movieland/ui/coming_soon/ComingSoonFragment.kt
@@ -198,12 +198,12 @@ class ComingSoonFragment : Fragment() {
     private suspend fun getBitmapFromUrl(imgUrl: String): Bitmap? {
         return try {
             val drawable = (
-                    (requireActivity() as MainActivity).imageLoader.execute(
-                        (requireActivity() as MainActivity).imageRequestBuilder.data(
-                            TMDB_CAST_IMAGE_BASE_URL_W185.plus(imgUrl)
-                        ).build()
-                    ) as SuccessResult
-                    ).drawable
+                (requireActivity() as MainActivity).imageLoader.execute(
+                    (requireActivity() as MainActivity).imageRequestBuilder.data(
+                        TMDB_CAST_IMAGE_BASE_URL_W185.plus(imgUrl)
+                    ).build()
+                ) as SuccessResult
+                ).drawable
             // Returning Bitmap
             (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
         } catch (e: Exception) {
